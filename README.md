8c9e21d7111a5651262000c7746d401df07083ff
youngDaLee <ldy0956@gmail.com>
2025-04-12 11:35:58 +0900

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple-stream-collector
+코틀린으로 제작한 스트림 콜렉터 라이브러리 입니다.
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "simple-stream-collector"
+
+include(
+    "simple-stream-collector-core",
+    "simple-stream-collector-spring"
+)
